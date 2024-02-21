ad06801fa04905d4e79ea2b7c3841c5086ce4793
Stanislaav Kozh <printks@yandex.ru>
2024-02-21 23:01:55 +0300

    Impl User and Task

--- src/main/java/com/example/tasklist/repository/UserRepository.java
+++ src/main/java/com/example/tasklist/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository {
 
-    Optional<User> findBuId(Long id);
+    Optional<User> findById(Long id);
 
     Optional<User> findByUsername(String username);
 
